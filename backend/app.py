8114e956234ec5d49847736374aceb5c5128b2be
sumankhalia <sumankhalia05@gmail.com>
2026-02-17 18:44:30 +0530

    CORS fix

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from main import run_swarm
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # for dev/demo (later restrict)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
     return {"status": "Swarm Cardio OS Running"}
-
 
 @app.get("/analyze")
 def analyze():
